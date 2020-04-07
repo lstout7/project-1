38f7fa0469318baf147935e7767b86b459ea3576
Logan Stout <lstout7@uoregon.edu>
2020-04-07 16:21:22 -0700

    Created HTML for die page, created stylesheet for die page, created js(incomplete) for die page, added second function into function.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,10 +1,33 @@
 //jshint esversion: 7
 
-let arrAvg = function(arr){
+//req. 3.b
+let arrAvg = (arr) => {
   var total = 0;
 for(var i = 0; i < arr.length; i++) {
     total += arr[i];
 }
 var avg = total / arr.length;
 return avg;
+};
+
+//req. 3.c
+let arrMax = (arr) => {
+var largest = 0;
+arr.forEach(function(elem){
+  if(largest < elem)
+  largest = elem;
+});
+
+console.log(largest);
+};
+
+//req. 3.d INCOMPLETE
+let sumEvents = (arr) => {
+  let sum = 0;
+  for(let i in arr){
+    if (i % 2 == 0){
+    sum += i;
+   }
+  }
+  return sum;
 };
